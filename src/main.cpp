08bbe52cbc71cadcf1edc49098a8f72bc86176ce
popica23 <andrei23pas@gmail.com>
2015-04-18 04:15:38 +0300

    Lel m8

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 int main()
 {
     std::cout << "Hello Ludum Dare 32!";
-    std::cout << "\n Hello again!\n";
+    std::cout << "\nHello again!\n";
     std::cout << "Hello m8.\n";
+    std::cout << "Man ... Man ...\n";
     return 0;
 }
